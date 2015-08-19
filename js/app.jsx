8394551260b88b5c43b0231a929dfe149d7281e8
dangitsvi <dangitsvi@gmail.com>
2015-08-18 21:47:16 -0700

    project setup

---
+++ js/app.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <main>
+        <h1>This is my reddit app dupicate</h1>
+      </main>
+    );
+  }
+});
+
+
+React.render(<App />, document.body);
